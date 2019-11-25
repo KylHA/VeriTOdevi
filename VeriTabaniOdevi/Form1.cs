fd74076c535d501940338122a3303c86f0b0f2dd
KylHA <programlamakitabi@gmail.com>
2019-11-26 01:36:58 +0300

    GirisFormu
    Kullanıcı giriş formu yapıldı ve sql bağlantısı hazırlandı kullanıcı oluşturma için form2 hazırlandı isimlendirme değiştirilecek

--- VeriTabaniOdevi/Form1.cs
+++ VeriTabaniOdevi/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace VeriTabaniOdevi
 {
+    
     public partial class Form1 : Form
     {
+        public bool Flag = false;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +22,62 @@ namespace VeriTabaniOdevi
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Helloworld");
+            try
+            {
+                if (!(textBox1.Text == string.Empty))
+                {
+                    if (!(textBox2.Text == string.Empty))
+                    {
+                        String str = "server=MYSQL80;database=windowapp;UID=VTM;password=2327";//Connect Hatası Veriyor Bakılacak
+                        String query = "select * from data where username = '" + textBox1.Text + "'and password = '" + this.textBox2.Text + "'";
+                        SqlConnection con = new SqlConnection(str);
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        SqlDataReader dbr;
+                        con.Open();
+                        dbr = cmd.ExecuteReader();
+                        int count = 0;
+                        while (dbr.Read())
+                        {
+                            count = count + 1;
+                        }
+                        if (count == 1)
+                        {
+                            this.Visible=false;
+                            Form2 f2 = new Form2(); //this is the change, code for redirect  
+                            f2.ShowDialog();
+                        }
+                        else if (count > 1)
+                        {
+                            MessageBox.Show("Duplicate username and password", "login page");
+                        }
+                        else
+                        {
+                            MessageBox.Show(" username and password incorrect", "login page");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(" password empty", "login page");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(" username empty", "login page");
+                }
+                // con.Close();  
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+        public void ShowForm()
+        { this.Visible = true; }
     }
 }
